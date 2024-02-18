049db995374dbf38e8f02ca53388c25477f7e180
Tony Vang <97489898+TonyVang559@users.noreply.github.com>
2024-02-18 15:42:29 -0800

    Add files via upload

--- Driver.java
+++ Driver.java
@@ -2,7 +2,7 @@ public class Driver {
     public static void main (String [] args) {
 
         //Here's my stuffs object
-        Stuff myStuff = new Stuff();
+        myStuff myStuff = new myStuff();
 
         //My stuff object information data
         myStuff.coffee = "\n" + "French Roast Ground Coffee (Medium Dark)";
@@ -10,10 +10,12 @@ public class Driver {
         myStuff.container = "\n" + "Bedroom trashcan";
         myStuff.fruit = "\n" + "Bananas and Avocados";
         myStuff.dogAge = 6;
+        myStuff.rice = "Jasmine rice";
 
         System.out.println("\n" + "My go to coffee is: " + myStuff.coffee);
         System.out.println("\n" + "My current collectible is: " + myStuff.collectible);
         System.out.println("\n" + "I have a: " + myStuff.container);
+        System.out.println("\n" + "I have and eat: " + myStuff.rice);
 
     }
 }
